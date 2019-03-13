9f3c8c8d0e403a63b6abce91ac71401c20061b20
zhf <930289986@qq.com>
2019-03-13 14:13:18 +0800

    Add user statuses

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,4 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 //微博
-Route::resource('statuses','StatusesController',['only' => ['store','only']]);
+Route::resource('statuses','StatusesController',['only' => ['store','destroy']]);
